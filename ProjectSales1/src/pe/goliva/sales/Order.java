a6ade8af5820632562b4f9c9fb513b57eed00ec7
G-Laptop <G-Laptop@DESKTOP-642IIKR>
2022-04-25 22:47:20 -0500

    create class

---
+++ ProjectSales1/src/pe/goliva/sales/Order.java
@@ -0,0 +1,11 @@
+package pe.goliva.sales;
+
+/**
+ *
+ * @author	: Giomar Oliva
+ * @date	: 25 abr 2022
+ * 
+ */
+public class Order {
+
+}
---
+++ ProjectSales1/src/pe/goliva/sales/test/TestSales.java
@@ -0,0 +1,19 @@
+package pe.goliva.sales.test;
+
+/**
+ *
+ * @author	: Giomar Oliva
+ * @date	: 25 abr 2022
+ * 
+ */
+public class TestSales {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
