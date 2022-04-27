bf0334d88682caf9b052534c8a9cec3af9bfe140
G-Laptop <G-Laptop@DESKTOP-642IIKR>
2022-04-26 19:19:39 -0500

    se añade objetos y se prueba la clase order

--- ProjectSales1/src/pe/goliva/sales/test/TestSales.java
+++ ProjectSales1/src/pe/goliva/sales/test/TestSales.java
@@ -13,10 +13,14 @@ public class TestSales {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Product product1 = new Product("Camisa",50);
-		System.out.println(product1.toString());	
+		Product product1 = new Product("Camisa	",50);
+		Product product2 = new Product("Pantalon	",100);
 		Client client1 = new Client("Giomar");
-		System.out.println(client1.toString());
+		Order order1 = new Order();
+		order1.addClient(client1);
+		order1.addProduct(product1, 1);
+		order1.addProduct(product2, 2);
+		order1.viewOrder();
 	}
 
 }
